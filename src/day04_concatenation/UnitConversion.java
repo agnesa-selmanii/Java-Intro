7d4e6006bb859fe6b227b1aa3c4c435b116cfae2
agnesaselmani03 <agnesa.selmani01@icloud.com>
2024-07-02 00:17:33 +0200

    Conversion of units

---
+++ src/day04_concatenation/UnitConversion.java
@@ -0,0 +1,18 @@
+package day04_concatenation;
+
+public class UnitConversion {
+    public static void main(String[] args) {
+        double gallons = 10;
+
+        double litters = gallons * 3.79;
+
+        System.out.println(gallons + " gallons is equal to " + litters + " litters");
+
+        double kg = 10.5;
+
+        double lb = kg * 2.2;
+
+        System.out.println(kg + " kilos is equal to " + lb + " pounds");
+
+    }
+}
