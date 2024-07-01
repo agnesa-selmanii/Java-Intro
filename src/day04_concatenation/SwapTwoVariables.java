06a86e0bf6fe87b08f544591507ce170bd5b4c64
agnesaselmani03 <agnesa.selmani01@icloud.com>
2024-07-02 00:14:48 +0200

    Swap teo variables without using addition variables

---
+++ src/day04_concatenation/SwapTwoVariables.java
@@ -0,0 +1,15 @@
+package day04_concatenation;
+
+public class SwapTwoVariables {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
