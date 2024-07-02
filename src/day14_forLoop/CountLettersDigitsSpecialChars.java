a0fec56d1b2956bd8d6e4b2a576b13bbf3849083
agnesaselmani03 <agnesa.selmani01@icloud.com>
2024-07-02 18:35:53 +0200

    count letters, digits, special chars using for loop

---
+++ src/day14_forLoop/CountLettersDigitsSpecialChars.java
@@ -0,0 +1,34 @@
+package day14_forLoop;
+
+public class CountLettersDigitsSpecialChars {
+    public static void main(String[] args) {
+        String str = "ABCD123#$@!";
+
+        String digits = "";
+        String letters = "";
+        String specialChars = "";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch >= '0' && ch <= '9') {
+                digits += ch;
+            } else if (ch >= 'A' && ch <= 'Z') {
+                letters += ch;
+            } else if (ch >= 'a' && ch <= 'z') {
+                letters += ch;
+            } else {
+                if (ch != ' ') {
+                    specialChars += ch;
+                }
+            }
+
+        }
+
+        System.out.println("digits = " + digits);
+        System.out.println("letters = " + letters);
+        System.out.println("specialChars = " + specialChars);
+
+    }
+}
