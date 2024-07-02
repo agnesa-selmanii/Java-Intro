634664e8e76f5c909bdb88b36226f59b282f803a
agnesaselmani03 <agnesa.selmani01@icloud.com>
2024-07-02 21:20:08 +0200

    Static intro

--- src/day26_statics/StaticIntro.java
+++ src/day26_statics/StaticIntro.java
@@ -1,14 +1,51 @@
 package day26_statics;
 
 public class StaticIntro {
-    //static: var, method, blocks, classes
-    public static String iOS="Apple";
-    public String brand;
+    public static String brand = "Apple"; //static: "Apple" is the brand of all the iphone objects
 
-    public void printInfo(){
-        System.out.println("iOS: "+iOS + " Brand: "+brand);
+    public String model; //instance: different models for iphone objects
+
+    public String color;
+
+    public double price;
+
+    public static String OS = "iOS"; // static: operating system is same fro all the iphone objects
+
+    public static String madeIn = "China";
+
+    public static boolean hasBattery = true;
+
+    public static boolean isTouchScreen = true;
+
+    public static boolean isExpensiveToFix = true;
+
+    public StaticIntro(String model, String color, double price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
     }
-    public static void printInfoStatic(){
-        System.out.println("iOS: "+iOS);
+
+
+
+    /*
+    public static void printPhoneInfo(){ // static only accepts statics
+        System.out.println("Model: " + model);
+        System.out.println("Color: " + color);
     }
+    */
+
+
+    public void printPhoneInfo() { // instance methods accepts both static and none static
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Color: " + color);
+        System.out.println("Price: " + price);
+    }
+
+
+    public static void printOperatingSystem(){
+        System.out.println("Operating System: " + OS);
+    }
+
+
 }
