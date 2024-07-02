242dc3fff24ad90e913cba5d066196601cf6bbf5
agnesaselmani03 <agnesa.selmani01@icloud.com>
2024-07-02 17:58:57 +0200

    Switch update

---
+++ src/day08_switch/JavaVersionUpdatesForSwitch.java
@@ -0,0 +1,53 @@
+package day08_switch;
+
+public class JavaVersionUpdatesForSwitch {
+    public static void main(String[] args) {
+        int day = 1;
+
+
+        /*
+        switch (day){
+
+            case 1,2 ,3 ,4 ,5:
+                System.out.println("Week Day");
+                break;
+
+            case 6,7:
+                System.out.println("Weekend");
+                break;
+
+            default:
+                System.out.println("No such a day");
+
+        }*/
+
+        System.out.println("----------------------------------");
+/*
+
+        int n = 12;
+
+        String month = switch (n) { // ==, equals()
+            case 1 -> "January";
+            case 2 -> "February";
+            case 3 -> "March";
+            case 4 -> "April";
+            case 5 -> "May";
+            case 6 -> "June";
+            case 7 -> "July";
+            case 8 -> "August";
+            case 9 -> "September";
+            case 10 -> "October";
+            case 11 -> "November";
+            case 12 -> "December";
+            default -> "No such a month";
+
+        };
+
+
+        String var ="Java Programming";
+
+        System.out.println("month = " + month);
+
+*/
+    }
+}
