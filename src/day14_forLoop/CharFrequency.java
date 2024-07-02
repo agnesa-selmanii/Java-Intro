8d8bb3304a6300a8092a8bb698698bdada4a07ee
agnesaselmani03 <agnesa.selmani01@icloud.com>
2024-07-02 18:52:41 +0200

    freqeuncy of chars, for loop used

---
+++ src/day14_forLoop/CharFrequency.java
@@ -0,0 +1,27 @@
+package day14_forLoop;
+
+import java.util.Scanner;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a String and a char");
+        String str = input.next();
+        char ch = input.next().charAt(0);
+
+        int frequency = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char eachChar = str.charAt(i);
+            if(eachChar == ch){
+                frequency ++;
+            }
+        }
+
+        System.out.println(frequency);
+
+        input.close();
+
+    }
+}
